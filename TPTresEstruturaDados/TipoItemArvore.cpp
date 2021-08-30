dca54149a24b067200df1254a8393a859ec31efa
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-08-30 10:52:15 -0300

    Add: Dados em inteiro

--- TPTresEstruturaDados/TipoItemArvore.cpp
+++ TPTresEstruturaDados/TipoItemArvore.cpp
@@ -17,6 +17,11 @@ void TipoItemArvore::SetDados(std::string valor)
     this->dados = valor;
 }
 
+void TipoItemArvore::SetDadosInt(int valor)
+{
+    this->dadosInt = valor;
+}
+
 void TipoItemArvore::SetNome(std::string valor)
 {
     this->nome = valor;
@@ -25,6 +30,11 @@ void TipoItemArvore::SetNome(std::string valor)
 std::string TipoItemArvore::GetDados()
 {
     return this->dados;
+}
+
+int TipoItemArvore::GetDadosInt()
+{
+    return this->dadosInt;
 }
 
 std::string TipoItemArvore::GetNome()
--- TPTresEstruturaDados/TipoItemArvore.h
+++ TPTresEstruturaDados/TipoItemArvore.h
@@ -6,13 +6,16 @@ class TipoItemArvore
 		TipoItemArvore();
 		TipoItemArvore(std::string dados, std::string nome);
 		void SetDados(std::string valor);
+		void SetDadosInt(int valor);
 		void SetNome(std::string valor);
 		std::string GetDados();
+		int GetDadosInt();
 		std::string GetNome();
 		void Imprime();
 	private:
 		std::string dados;
 		std::string nome;
+		int dadosInt;
 
 		friend class Fila;
 		friend class TipoCelula;

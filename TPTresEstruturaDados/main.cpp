5481c90fc20143544772fefefa074d56365cbac1
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-08-30 10:52:08 -0300

    Add: Leitura de arquivo e preencher arvore

--- TPTresEstruturaDados/main.cpp
+++ TPTresEstruturaDados/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include "ArvoreBinaria.h"
+#include "TipoItemArvore.h"
 
-int main()
+using namespace std;
+
+void PreencheVetor(const char caminho[], ArvoreBinaria* arvore) {
+	char* result;
+	char Linha[100];
+	FILE* arquivo = fopen(caminho, "rt");
+	int aux = 0;
+	int tamanho = *fgets(Linha, 100, arquivo) - 48;
+	while (!feof(arquivo) && aux < tamanho)
+	{
+		result = fgets(Linha, 100, arquivo);
+		if (result == NULL)break;
+		string s = result;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == ' ') {
+				TipoItemArvore* tipoItem = new TipoItemArvore();
+				tipoItem->SetNome(s.substr(0, i));
+				tipoItem->SetDados(s.substr(i + 1, s.size()));
+				tipoItem->SetDadosInt(stoi(s.substr(i + 1, s.size())));
+				arvore->Insere(*tipoItem);
+				break;
+			}
+		}
+		aux++;
+	}
+	fclose(arquivo);
+}
+
+int main(int argc, const char* argv[])
 {
-    std::cout << "Hello World!\n";
+	ArvoreBinaria* arvore = new ArvoreBinaria();
+	PreencheVetor(argv[1], arvore);
 }

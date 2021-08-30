4b1ec01ced3df828e2f374d612c9e05c6d0345dc
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-08-30 10:12:25 -0300

    Add: Classe string services com metodo para comparar strings;

---
+++ TPTresEstruturaDados/StringServices.cpp
@@ -0,0 +1,18 @@
+#include "StringServices.h"
+
+StringServices::StringServices()
+{
+}
+
+bool StringServices::VemAntes(std::string primeira, std::string segunda)
+{
+	int tamanho = primeira.size() > segunda.size() ?
+		segunda.size() : primeira.size();
+
+	for (int i = 0; i < tamanho; i++) {
+		if (primeira[i] != segunda[i]) {
+			return primeira[i] < segunda[i];
+		}
+	}
+	return primeira.size() < segunda.size();
+}
---
+++ TPTresEstruturaDados/StringServices.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+class StringServices
+{
+	public:
+		StringServices();
+		bool VemAntes(std::string primeira, std::string segunda);
+};
+
